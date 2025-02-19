b356ff49495011c0df47906cab8f09d08df20eb8
Valmir Paiva Stachin <valmirpaivacpm@gmail.com>
2025-02-19 13:11:45 -0300

    feat: finish aula 01: project setup

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,29 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    "/subscriptions",
+    {
+      schema: {
+        summary: "Subscribes someone to the event",
+        tags: ["subscriptions"],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string().email(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+
+      return reply.status(201).send({ name, email });
+    }
+  );
+};
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { fastifyCors } from "@fastify/cors"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
+import { fastify } from "fastify"
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod"
+import { env } from "./env"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// serialização de dados antes de enviar para o frontend
+app.setSerializerCompiler(serializerCompiler)
+// validação de dados
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect Node.js API",
+      version: "0.0.1",
+    },
+  },
+
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }, () => {
+  console.log(`Server is running on port ${env.PORT}`)
+})
